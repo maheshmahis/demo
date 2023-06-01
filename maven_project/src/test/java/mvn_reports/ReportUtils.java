53a54cb2bf05cbb8eb98a15a0c7b36db80027a47
mahesh <maheshs.in.in@gmail.com>
2023-06-01 21:42:59 +0530

    Added result method

--- maven_project/src/test/java/mvn_reports/ReportUtils.java
+++ maven_project/src/test/java/mvn_reports/ReportUtils.java
@@ -1,5 +1,23 @@
 package mvn_reports;
 
+
+import com.relevantcodes.extentreports.ExtentReports;
+
 public class ReportUtils {
 
+	public ExtentReports reports(String reoprtpath) {
+		try {
+			
+			}
+			
+		catch (Exception e) {
+			System.out.println(e);
+			
+		}
+		finally {
+			
+			
+		}
+		return null;
+	}
 }
